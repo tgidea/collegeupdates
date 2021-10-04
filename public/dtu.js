ed750c6bb7ea6968f1c3deae0375f9b6bd5beb04
Tushar <tushargoyal85709@gmail.com>
2021-10-04 23:38:23 +0530

    add dtu

---
+++ public/dtu.js
@@ -0,0 +1,42 @@
+const showData = (articles) => {
+    const artitem = articles;
+
+    var items_list = document.getElementById('items-list');
+    for (var i = 0; i < artitem.length; i++) {
+        var output = "";
+        if (artitem[i].finalurl == "undefined") {
+            output += `
+            <div class="card text-center border-danger">
+                 <div class="card-body ">
+                     <p class="card-text card-header ">${artitem[i].title}</p>
+                </div>
+             </div>`
+        }
+        else{
+        output += `
+    <div class="card text-center border-danger">
+     <div class="card-body ">
+     <p class="card-text card-header ">${artitem[i].title}</p>
+    <a href="${artitem[i].finalurl}" class="btn btn-danger">PDF</a>
+       </div>
+     </div>`
+        }
+        items_list.innerHTML += output;
+    }
+}
+const fetching = () => {
+    try {
+        fetch("dtu.json")
+            .then(function (resp) {
+                return resp.json();
+            })
+            .then(function (data) {
+                showData(data)
+            });
+    }
+    catch (err) {
+        console.log(err);
+    }
+}
+document.addEventListener('DOMCONTENTLOADED', fetching());
+// fetch();
--- src/app.js
+++ src/app.js
@@ -21,6 +21,7 @@ app.get('/', (req, res) => {
 
 app.use(express.static(path.join(__dirname, "../public")));
 app.get('/jcbose', (req, res) => {
+    
     try {
         axios('https://www.jcboseust.ac.in/content/all_notices/general-notices')
             .then(res => {
@@ -90,6 +91,56 @@ app.get('/dcrust', (req, res) => {
         }
     }
     res.sendFile(path.join(__dirname, "../public", 'dcrust.html'));
+})
+
+app.get('/dtu', (req, res) => {
+    
+    // async () => {
+        try {
+            var i = 0;
+             axios('http://dtu.ac.in')
+                .then(res => {
+                    
+                    const html = res.data;
+                    const $ = cheerio.load(html);
+                    const articles = [];
+                    $('#tab4', html).each(function () {
+                        
+                        $(this).find('li').each(function () {
+                            $(this).find('a').each(function () {
+                                
+                                const url = $(this).attr('href');
+                                var finalurl='undefined';
+                                const title = $(this).text();
+                                if (url != undefined) {
+                                    if (url[0] == 'h')
+                                        var finalurl = `${url}`;
+                                    else {
+                                        const len = url.length;    
+                                        const url2 = url.slice(1,len);
+                                         finalurl = `http://dtu.ac.in${url2}`;
+                                    }
+                                }
+                                i++;
+                                if (i < 50) {
+                                    articles.push({ title, finalurl });
+                                }
+                            })
+                        })
+                        // }
+                    });
+                    
+                    fs.writeFile((path.join(__dirname, '../public/', 'dtu.json')), JSON.stringify(articles, null, 2), (err) => {
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
+                })
+                .catch(err => console.log(err));
+        }
+        catch (err) { console.log(err) }
+    // }
+    res.sendFile(path.join(__dirname, "../public", 'dtu.html'));
 })
 
 app.get('*',(req,res)=>{
